ad1dac63216d66a9ea0a4de659ba7684e6b90e08
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-12-27 21:02:01 +0900

    Remove

--- packages/oregexp-engine/src/tests/nfa.spec.ts
+++ packages/oregexp-engine/src/tests/nfa.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
-import { e, Nfa } from "../nfa.js";
+import { Nfa } from "../nfa.js";
 
 describe("nfa", () => {
   it("validates invalid nfa states", () => {
