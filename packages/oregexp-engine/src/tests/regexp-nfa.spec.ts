189ddae75cb2ed89ba0b5cc6932fcd5cc1cef84a
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-12-27 23:25:36 +0900

    Add more tests

--- packages/oregexp-engine/src/tests/regexp-nfa.spec.ts
+++ packages/oregexp-engine/src/tests/regexp-nfa.spec.ts
@@ -3,13 +3,23 @@ import assert from "node:assert/strict";
 import { parse } from "@sosukesuzuki/oregexp-parser";
 import { createNfaFromAst } from "../regexp-nfa.js";
 
+const prepare = (regexp: string) => {
+  const ast = parse(regexp);
+  const nfa = createNfaFromAst(ast);
+  return (value: string) => nfa.run(value);
+};
+
 describe("regexp nfa", () => {
-  it("concat of literals", () => {
-    const regexp = "ab";
-    const ast = parse(regexp);
-    const nfa = createNfaFromAst(ast);
-    assert(nfa.run("ab"));
-    assert(!nfa.run("a"));
-    assert(!nfa.run("abc"));
+  it(`regexp /ab/`, () => {
+    const check = prepare("ab");
+    assert(check("ab"));
+    assert(!check("a"));
+    assert(!check("abc"));
+  });
+  it(`regexp /abcdefg/`, () => {
+    const check = prepare("abcdefg");
+    assert(check("abcdefg"));
+    assert(!check("afffff"));
+    assert(!check("abasdlfkjlc"));
   });
 });
