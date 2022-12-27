bd4ab01983640c8aaae2f5cf991312ce57319eec
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-12-27 18:33:25 +0900

    Add tests

--- packages/oregexp-engine/src/tests/nfa.spec.ts
+++ packages/oregexp-engine/src/tests/nfa.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
-import { Nfa } from "../nfa.js";
+import { e, Nfa } from "../nfa.js";
 
 describe("nfa", () => {
   it("validates invalid nfa states", () => {
@@ -19,5 +19,28 @@ describe("nfa", () => {
         /* transition */ {}
       );
     });
+  });
+
+  it("works with the simplest NFA", () => {
+    // 正規表現 "abc" と等価の NFA
+    const nfa = new Nfa(
+      [
+        { label: "q0", initial: true },
+        { label: "q1" },
+        { label: "q2" },
+        { label: "q3", accepted: true },
+      ],
+      {
+        q0: { a: ["q1"], [e]: [] },
+        q1: { b: ["q2"], [e]: [] },
+        q2: { c: ["q3"], [e]: [] },
+      }
+    );
+    nfa.read("a");
+    assert(!nfa.accepted);
+    nfa.read("b");
+    assert(!nfa.accepted);
+    nfa.read("c");
+    assert(nfa.accepted);
   });
 });
