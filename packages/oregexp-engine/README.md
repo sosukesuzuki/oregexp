fb799eb02e417c1e38348d42a763b170f57a45dd
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-12-31 09:27:04 +0900

    Add API

--- packages/oregexp-engine/README.md
+++ packages/oregexp-engine/README.md
@@ -1,1 +1,12 @@
 # `@sosukesuzuki/oregexp-engine`
+
+## API
+
+```ts
+import { createNewRegexp } from "@sosukesuzuki/oregexp-engine";
+
+const test = createNewRegexp("(a|b)*ccc");
+
+test("hoge"); // false
+test("ababababccc"); // true
+```
--- packages/oregexp-engine/src/index.ts
+++ packages/oregexp-engine/src/index.ts
@@ -1,5 +1,8 @@
 import { parse } from "@sosukesuzuki/oregexp-parser";
+import { createNfaFromAst } from "./regexp-nfa.js";
 
-export function main() {
-  //
+export function createNewRegex(regexp: string) {
+  const ast = parse(regexp);
+  const nfa = createNfaFromAst(ast);
+  return (value: string) => nfa.run(value);
 }
